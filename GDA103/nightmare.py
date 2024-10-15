0adc6b6c6681db2c1cb431487fb5b8de8f61fdd6
Bibi <bibidev99@gmail.com>
2024-10-15 10:20:46 +0200

    Error exception handling

--- GDA103/nightmare.py
+++ GDA103/nightmare.py
@@ -8,23 +8,29 @@ def konvertera_enheter(värde, från_värde, till_värde):
         'TB': 10**12
     }
 
-    # Konverterar till bitar så att vi kan konverta till nytt värde baserat på enheternas exponent
+    # Konverterar till bitar för att konverta till nytt värde baserat på enheternas exponent
     till_bytes = värde * enheter[från_värde]
 
-    #
+    # Konverta från bytes till värde med NY enhet
     konverterat_värde = till_bytes / enheter[till_värde]
 
     # Rundar upp till decimaler
     return round(konverterat_värde, 2)
 
-
 def main():
-    print("Ange vilken enhet du vill konvertera från och till, B, KB, MB, GB, TB")
-    från_v = input("Ange värde du vill konverta från: ").upper()
-    till_v = input("Ange värde du vill konverta till: ").upper()
-    n = int(input("Ange storlek: "))
+    while True:
+        print("Ange vilken enhet du vill konvertera från och till, B, KB, MB, GB, TB")
+        från_v = input("Ange värde du vill konverta från: ").upper()
+        till_v = input("Ange värde du vill konverta till: ").upper()
+        n = int(input("Ange storlek: "))
 
-    print(konvertera_enheter(värde=n, från_värde=från_v, till_värde=till_v), till_v)
+        try:
+            if från_v or till_v in konvertera_enheter(n, från_värde=från_v, till_värde=till_v):
+                print(konvertera_enheter(n, från_v, till_v), till_v)
+                break
+        except:
+            print("Värdet existerar inte!")
+            break
 
 if __name__ == "__main__":
     main()
